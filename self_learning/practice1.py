329096bdf2de174e80de18aa34687cfe665d6f76
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-03-15 18:09:33 +0500

    Added practice1.py for basic arithmetic operations and comparisons

---
+++ self_learning/practice1.py
@@ -0,0 +1,25 @@
+# sum of 2 num by user
+
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+print(f"Sum of {num1} and {num2} is : {num1+num2}")
+
+# area of square
+
+side = float(input("Enter side of square:"))
+
+print(f"Area of square of side {side} is : {side*side}")
+
+# avg of two floating numbers
+
+fNum1 = float(input("Enter first floating num:"))
+fNum2 = float(input("Enter second floating num:"))
+
+print(f"Average of numbers {fNum1} and {fNum2} is {(fNum1+fNum2)/2}")
+
+# checking grater number
+cNum1 = int(input("Enter first number:"))
+cNum2 = int(input("Enter second number:"))
+
+print(f"{cNum1} >= {cNum2} = {cNum1 >= cNum2}")
