cbb6a6b68ac51fdf32ed23a70fbd298e9a0f06f8
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-03-16 13:32:10 +0500

    Added practice2.py for string length and character occurrence examples

---
+++ self_learning/practice2.py
@@ -0,0 +1,6 @@
+# print length of users name
+username = input("Please enter your full name : ")
+print(f"Your name {username} consist of {len(username)} characters")
+# find occurance of o in a string given by user
+string = input("Enter a string : ")
+print(f"Your string consist {string.count("o")} o's")
