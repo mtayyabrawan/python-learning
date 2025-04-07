36f7a8a89361bf796af044a14bab6a2bbd823fff
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-07 15:07:12 +0500

    added for loop with range

---
+++ self_learning/loops.py
@@ -0,0 +1,25 @@
+# while loops
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+i = 0
+while i <= 100:
+    print("Muhammad Tayyab Raza Awan")
+    i += 1
+
+# for loop
+
+names = ["Ali", "Ahmad", "Umar", "Abdullah", "Usman"]
+
+for name in names:
+    print(name)
+
+my_name = "Muhammad Tayyab Raza Awan"
+
+for chars in my_name:
+    print(chars)
+
+for n in range(1, 11):
+    print(n)
