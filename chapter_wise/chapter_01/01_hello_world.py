edd0d0c0b51d32b77cc3be968d54995f589e9be1
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-10-15 15:52:21 +0500

    added initial Python scripts for basic concepts: hello world, built-in modules, and external modules

---
+++ chapter_wise/chapter_01/01_hello_world.py
@@ -0,0 +1,7 @@
+# Following line prints Hello world! to console and this line is a single line comment that will be ignored by python interpreter
+
+print("Hello world!")
+
+'''
+This is th multiline comment that is used to comment multiple lines
+'''
