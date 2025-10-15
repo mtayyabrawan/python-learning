62f61a29650f1b3cc874962e04870a4e626816bf
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-10-15 16:01:12 +0500

    completed ch_1 ps

---
+++ chapter_wise/chapter_01/practice_set/problem_03.py
@@ -0,0 +1,5 @@
+import os # imported os module
+
+path = "./chapter_wise/chapter_01/practice_set" # define path of directory
+
+print("List of files and folders in this directory is \n",os.listdir(path)) # list all files and folders in the directory
